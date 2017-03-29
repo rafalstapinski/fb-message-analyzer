cf0ac067cefeb3243b5f38882cdbf5ef2402e521
Rafal Stapinski <stapinskirafal@gmail.com>
2017-03-29 18:24:31 -0400

    wordfreq changes

--- wordfreq.py
+++ wordfreq.py
@@ -12,12 +12,9 @@ def compareItems((w1,c1), (w2,c2)):
     else:
         return 1
 
-def main():
-    print "This program analyzes word frequency in a file"
-    print "and prints a report on the n most frequent words.\n"
+def main(fname, nwords):
 
     # get the sequence of words from the file
-    fname = raw_input("File to analyze: ")
     text = open(fname,'r').read()
     text = string.lower(text)
     for ch in '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~':
@@ -29,12 +26,8 @@ def main():
     for w in words:
         counts[w] = counts.get(w,0) + 1
 
-    # output analysis of n most frequent words.
-    n = input("Output analysis of how many words? ")
+    # return analysis of n most frequent words.
     items = counts.items()
     items.sort(compareItems)
-    for i in range(n):
-        print "%-10s%5d" % items[i]
 
-if __name__ == '__main__':
-    main()
+    return items[:nwords]
